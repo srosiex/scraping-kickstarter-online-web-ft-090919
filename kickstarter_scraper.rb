7d4f239a3eca2128b7af33081863402188a3d2bf
Sarah H <sarahrose.henderson@gmail.com>
2019-09-29 22:30:20 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,3 +12,6 @@ end
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
+#image link: project.css("div.project-thumbnail a img").attribute("src").value
+#location: project.css("span.location-name")
+#funded percent: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
