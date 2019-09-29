a05dc0067d6969acac27cfa078431716b01eeb5a
Sarah H <sarahrose.henderson@gmail.com>
2019-09-29 22:37:05 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,7 +15,13 @@ end
 
     kickstarter.css("li.project.grid_4").each do |project|
       title = project.css("h2.bbcard_name strong a").text
-      project[title.to_sym] = {}
+      project[title.to_sym] = {
+        
+              :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+              :description => project.css("p.bbcard_blurb").text,
+              :location => project.css("ul.project-meta span.location-name").text,
+              :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      }
   end
   projects
 end
